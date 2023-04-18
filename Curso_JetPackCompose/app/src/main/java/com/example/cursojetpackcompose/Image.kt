7310580faa75e4a41fe7ea8fe508133218f98ac2
Cora <melos4dev@gmail.com>
2023-04-18 19:55:57 +0200

    ProgressBar terminadas

--- Curso_JetPackCompose/app/src/main/java/com/example/cursojetpackcompose/Image.kt
+++ Curso_JetPackCompose/app/src/main/java/com/example/cursojetpackcompose/Image.kt
@@ -1,13 +1,22 @@
 package com.example.cursojetpackcompose
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material.icons.rounded.StarBorderPurple500
+import androidx.compose.material.icons.rounded.StarHalf
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposableOpenTarget
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -27,9 +36,22 @@ fun MyImageAdvance() {
     Image(
         painter = painterResource(id = R.drawable.ic_launcher_background),
         contentDescription = "Ejemplo avanzado",
-        modifier = Modifier.clip(CircleShape)
+        modifier = Modifier
+            .clip(CircleShape)
+            .border(5.dp, Color.Red, CircleShape)
 
     )
+}
+
+@Composable
+fun MyIcon() {
+    Icon(
+        imageVector = Icons.Rounded.StarHalf,
+        contentDescription = "Icono",
+        tint = Color.Red
+
+    )
+
 }
 
 
@@ -43,4 +65,10 @@ fun PreviewImagenesCirculares() {
 @Composable
 fun PreviewImageAdvance() {
     MyImageAdvance()
+}
+
+@Preview(name = "Ejemplo Iconos", showSystemUi = true, showBackground = true)
+@Composable
+fun PreviewImageIcon() {
+    MyIcon()
 }
---
+++ Curso_JetPackCompose/app/src/main/java/com/example/cursojetpackcompose/ProgressBar.kt
@@ -0,0 +1,116 @@
+package com.example.cursojetpackcompose
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+//get y set de value para el rememberSaveable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun MyProgressBar() {
+    Column(
+        Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(color = Color.Magenta, strokeWidth = 10.dp)
+        LinearProgressIndicator(
+            modifier = Modifier.padding(top = 16.dp),
+            color = Color.Magenta,
+            backgroundColor = Color.Transparent
+        )
+    }
+
+}
+
+
+@Composable
+fun MyProgressBarAdvance() {
+    var showLoading by rememberSaveable { mutableStateOf(false) }
+
+    Column(
+        Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (showLoading) {
+            CircularProgressIndicator(color = Color.Magenta, strokeWidth = 10.dp)
+            LinearProgressIndicator(
+                modifier = Modifier.padding(top = 16.dp),
+                color = Color.Magenta,
+                backgroundColor = Color.Transparent
+            )
+        }
+        Button(onClick = { showLoading = !showLoading }) {
+
+            Text(text = "Cargar perfil")
+
+        }
+
+    }
+
+}
+
+@Composable
+fun MyProgressBarSuperAdvance() {
+    var progressSatatus by rememberSaveable { mutableStateOf(0f) }
+
+    Column(
+        Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(progress = progressSatatus) //estamos diciendo que esta a la mitad
+        // podemos sustituir el circular por una LinearProgressIndicator para que sea la barra
+
+        Row(Modifier.fillMaxWidth()) {
+            Button(onClick = { progressSatatus += 0.1f }) {
+                Text(text = "Incrementar")
+            }
+
+            Button(onClick = { progressSatatus -= 0.1f }) {
+                Text(text = "Reducir")
+
+            }
+        }
+    }
+
+}
+
+
+
+@Preview(name = "Ejemplo progressBar", showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewProgressBar() {
+    MyProgressBar()
+}
+
+@Preview(name = "Ejemplo progressBar", showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewProgressBarAdvance() {
+    MyProgressBarAdvance()
+}
+
+@Preview(name = "Ejemplo progressBar", showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewProgressBarSuperAdvance() {
+    MyProgressBarSuperAdvance()
+}
